0bfbaa6eb402014f63b09f83d3bde5533da0dcff
Adriano Elias <63488275+nanoesouza@users.noreply.github.com>
2022-07-26 18:06:20 -0300

    add packages

--- fedora.sh
+++ fedora.sh
@@ -4,6 +4,96 @@
 ## Variables
 host='workstation'
 
+sys_pkg=(
+  alacritty
+  kitty
+  ripgrep
+  parallel
+  pwgen
+  git
+  ranger
+  vim
+  python3-neovim
+  dejavu-fonts-all
+  jetbrains-mono-fonts-all
+  fira-code-fonts
+  mozilla-fira-fonts-common
+  bat
+  exa
+  shellcheck
+  fish
+  zsh
+  fd-find
+  git-crypt
+  tldr
+  z
+  nmap
+  sshpass
+  python3-pip
+  strace
+  htop
+  bpytop
+  awscli
+  android-tools
+  neofetch
+  gparted
+  barrier
+
+)
+
+usr_pkg=(
+  kvantum
+  latte-dock
+  steam
+  discord
+  easyeffects
+  vlc
+  deja-dup
+  thunderbird
+  qbittorrent
+  bottles
+)
+
+wm_pkg=(
+  nitrogen
+  rofi
+  dunst
+  redshift
+  brightnessctl
+  lxappearance-gtk3
+  flameshot
+  i3lock
+  picom
+  julietaula-montserrat
+  awesome
+)
+
+wrk_pkg=(
+  ansible
+  ansible-lint
+  virtualbox
+  vagrant
+  python3-virtualenv
+  python3-virtualenvwrapper
+  remmina
+  remmina-plugins-rdp
+  remmina-plugins-vnc
+)
+
+flatpaks=(
+  com.bitwarden.desktop
+  com.mattermost.Desktop
+  com.brave.Browser
+  com.anydesk.Anydesk
+  md.obsidian.Obsidian
+)
+
+rpms=(
+  https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors.x86_64.rpm
+  https://desktop.docker.com/linux/main/amd64/docker-desktop-4.10.1-x86_64.rpm?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+)
+
+
 ## Sudo Config
 sudo_config(){
   sudo sed -i '110s/# %wheel/%wheel/' /etc/sudoers
@@ -48,13 +138,44 @@ install_codecs(){
   sudo dnf groupupdate -y sound-and-video
 }
 
+notion_enhanced(){
+  sudo echo  "[notion-repackaged]\
+              name=notion-repackaged\
+              baseurl=https://yum.fury.io/notion-repackaged/\
+              enabled=1\
+              gpgcheck=0" >> /etc/yum.repos.d/notion-repackaged.repo
+  
+  sudo dnf install notion-app-enhanced
+}
+
+vscode(){
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf check-update
+  sudo dnf install -y code
+}
+
 ## Package Install
+system_update(){
+  sudo dnf upgrade -y
+}
+
+install_usr_pkg(){
+  sudo dnf install -y $usr_pkg
+}
 ## Packages Config (awesome/qtile, backup restore)
 
 sudo_config
 dnf_tweaks
 enable_fusion
-set-_hostname
+set_hostname
 dnf_update
 flathub
 install_codecs
+system_update
+notion_enhanced
+vscode
+install_usr_pkg
+
+## TODO
+# Dotfiles (Clone as bare repo, set up the aliases, checkout to the config)
