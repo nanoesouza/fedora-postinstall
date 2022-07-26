cb8420a08084b6e0e496a5909d5356130551b9bf
Adriano Elias <63488275+nanoesouza@users.noreply.github.com>
2022-07-26 12:58:42 -0300

    add confirmation to dnf groupupdate

--- fedora.sh
+++ fedora.sh
@@ -44,8 +44,8 @@ flathub(){
 }
 
 install_codecs(){
-  sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-  sudo dnf groupupdate sound-and-video
+  sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+  sudo dnf groupupdate -y sound-and-video
 }
 
 ## Package Install
