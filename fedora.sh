b3f1a120f096ec0d644b75d5f30f3a7944bcfa66
nanoesouza <github@nano.slmail.me>
2022-07-26 12:39:33 -0300

    auto accepting some settings

--- fedora.sh
+++ fedora.sh
@@ -14,7 +14,7 @@ dnf_tweaks(){
 
 enable_fusion(){
  sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
- sudo dnf groupupdate core
+ sudo dnf groupupdate core -y
 }
 
 ## General Config (user/system)
